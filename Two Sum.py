c16e5a756d376200dea9637f9c79f30ddf773fea
= <ajfunari@gmail.com>
2021-09-20 11:55:27 -0400

    Two Sum

---
+++ Two Sum.py
@@ -0,0 +1,18 @@
+class Solution1:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return([i,j])
+
+class Solution2:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        seen = {}
+
+        for i, num in enumerate(num):  #enumerate -> returns index and value
+            if target - num in seen:
+                return([seen][target - num], i)
+            elif num not in seen:
+                seen[num] = i 
